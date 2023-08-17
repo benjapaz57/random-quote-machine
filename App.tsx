f759f4aa9d4c9161c3a6c791755619de4c26db50
benjapaz57 <89224883+benjapaz57@users.noreply.github.com>
2023-08-17 11:53:19 -0300

    asdddd

---
+++ App.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import quotes from "./assets/quotes.json"
+import {FaTwitter, FaQuoteLeft, FaQuoteRight} from 'react-icons/fa'
+import './App.css'
+
+interface Quote{
+  quote: string,
+  author: string
+}
+
+const getRandomQuote = (): Quote => {
+  return quotes[Math.floor(Math.random() * quotes.length)]
+}
+
+function App() {
+  const [quote, setQuote] = useState<Quote>(getRandomQuote());
+  const changeQuote = () => {
+    setQuote(getRandomQuote());
+  }
+
+
+  return <div className='background'>
+    <div id="quote-box">
+      <div className='quote-content'>
+        <FaQuoteLeft size='10' />
+        <h2 id="text">{quote.quote}</h2>
+        <FaQuoteRight size='10' />
+        <h4 id="author">--{quote.author}--</h4>
+      </div>
+      <div className="buttons">
+        <a href="twitter.com/intent/tweet" id='tweet-quote' target='blank'>
+          <FaTwitter size='30'/>
+        </a>
+        <button id="new-quote" onClick={changeQuote}>New Quote</button>
+      </div>
+    </div>
+  </div>
+}
+
+export default App
